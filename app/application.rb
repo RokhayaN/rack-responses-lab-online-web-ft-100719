537828369f6cb7140b14983509fbabd5270e4f9b
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-24 14:47:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,10 @@ class Application
 
     resp.write "#{time}"
 
-    if time < = 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!" 
-    end
+      if time < = 12
+        resp.write "Good Morning!"
+      else
+        resp.write "Good Afternoon!" 
+      end
     resp.finish 
-end
+  end
