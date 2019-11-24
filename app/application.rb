fbfba1c43a9c5c661d0fe39b751a905fd7b49e1c
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-24 14:32:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def App(env)
     resp = Rack::Response.new
     time =Time.now
-    if time <= 12
+    if time < = 12
       resp.write "Good Morning!"
     else 
        resp.write "Good Afternoon!"
